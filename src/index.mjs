e7d940a2073a22f80a8c2ebdc5bc7df7b28d7a62
Kirill <bloodwingest@gmail.com>
2023-06-26 20:42:17 +0300

    feat: add goodbye

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,9 @@
+import { createInterface } from 'readline';
+const rline = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 const startManager = async () => {
   const args = process.argv.slice(2);
   const usernameArg = args
@@ -5,7 +11,16 @@ const startManager = async () => {
     return el.startsWith('--username');
   })
   const username = usernameArg ? usernameArg.split('=')[1] : 'anonymous user'
-  process.stdout.write(`Welcome to the File Manager, ${username}!`);
+  process.stdout.write(`Welcome to the File Manager, ${username}!\n`);
+  process.stdout.write('Enter your command: ');
+  rline.on('line', (text) => {
+    if (text === '.exit'){
+      process.exit();
+    }
+    console.log(text);
+  });
+
+  process.on('exit', () => process.stdout.write(`\nThank you for using File Manager, ${username}, goodbye!`))
 };
 
 startManager();
