fe37043dcb140e106c34ae68803084f6458f1b1c
Kirill <bloodwingest@gmail.com>
2023-06-27 03:32:18 +0300

    feat: add os functions

--- src/index.mjs
+++ src/index.mjs
@@ -10,6 +10,7 @@ import rn from './rn.mjs';
 import cp from './cp.mjs';
 import mv from './mv.mjs';
 import del from './del.mjs';
+import ops from './ops.mjs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export let currentSysDirectory = homedir();
@@ -80,27 +81,36 @@ const startManager = async () => {
         }
         rn(text.split(' ')[1], text.split(' ')[2]);
         break;
-        case 'cp':
-          if (text.split(' ')[1] === undefined || text.split(' ')[2] === undefined) {
-            process.stdout.write(`Invalid input. If you want to copy a file, please type copy filename dirname\n`);
-            break;
-          }
-          cp(text.split(' ')[1], text.split(' ')[2]);
+      case 'cp':
+        if (text.split(' ')[1] === undefined || text.split(' ')[2] === undefined) {
+          process.stdout.write(`Invalid input. If you want to copy a file, please type copy filename dirname\n`);
           break;
-          case 'mv':
-            if (text.split(' ')[1] === undefined || text.split(' ')[2] === undefined) {
-              process.stdout.write(`Invalid input. If you want to move a file, please type mv filename dirname\n`);
-              break;
-            }
-            mv(text.split(' ')[1], text.split(' ')[2]);
-            break;
-          case 'del':
-            if (text.split(' ')[1] === undefined) {
-              process.stdout.write(`Invalid input. If you want to delete a file, please type del filename filepath\n`);
-              break;
-            }
-            del(text.split(' ')[1]);
-            break;
+        }
+        cp(text.split(' ')[1], text.split(' ')[2]);
+        break;
+      case 'mv':
+        if (text.split(' ')[1] === undefined || text.split(' ')[2] === undefined) {
+          process.stdout.write(`Invalid input. If you want to move a file, please type mv filename dirname\n`);
+          break;
+        }
+        mv(text.split(' ')[1], text.split(' ')[2]);
+        break;
+      case 'del':
+        if (text.split(' ')[1] === undefined) {
+          process.stdout.write(`Invalid input. If you want to delete a file, please type del filename filepath\n`);
+          break;
+        }
+        del(text.split(' ')[1]);
+        break;
+      case 'os':
+        if (text.split(' ')[1] === undefined) {
+          process.stdout.write(
+            `Invalid input. If you want to learn some info about your system please use this command with args\n`
+          );
+          break;
+        }
+        ops(text.split(' ')[1]);
+        break;
       default:
         process.stdout.write(`You are currently in ${currentSysDirectory}\n`);
         console.log('Invalid input');
